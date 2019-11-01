a677d702e2683ce98209f362ca132a0c17965f1c
fadi <fadi@soajs.org>
2019-11-01 13:54:52 +0200

    fixed test cases

--- test/unit/lib/drivers/facebook.js
+++ test/unit/lib/drivers/facebook.js
@@ -5,57 +5,55 @@ const driver = helper.requireModule('./lib/drivers/facebook.js');
 const assert = require('assert');
 
 describe("Unit test for: driver - facebook", function () {
-    let req = {
-        "soajs": {
-            "inputmaskData": {
-                "strategy": "facebook"
-            },
-            "servicesConfig": {
-                "urac": {
-                    "passportLogin": {
-                        "facebook": {
-                            "clientID": "331502413866510",
-                            "clientSecret": "1a07a7eb9c9884dc5d148106ede830b2",
-                            "callbackURL": "http://local-widget.com/urac/login/success?mode=facebook"
-                        }
-                    }
-                }
-            }
-        }
-    };
-    it("test - init", function (done) {
-        driver.init(req, (error, data) => {
-            assert.ok(data);
-            done();
-        });
-    });
-    it("test - mapProfile", function (done) {
-        let user = {
-            "profile": {
-                "_json":
-                    {
-                        "first_name": "tony",
-                        "last_name": "hage",
-                        "email": "antoine@soajs.org"
-                    },
-                "id": "123456789"
-            }
-        };
-        driver.mapProfile(user, (error, profile) => {
-            assert.ok(profile);
-            assert.equal(profile.email, "antoine@soajs.org");
-            done();
-        });
-    });
-    it("test - preAuthenticate", function (done) {
-        driver.preAuthenticate(req, (error) => {
-            done();
-        });
-    });
-    it("test - updateConfig", function (done) {
-        driver.updateConfig({"conf": "1"}, (error, config) => {
-            assert.equal(config.conf, "1");
-            done();
-        });
-    });
+	let req = {
+		"soajs": {
+			"inputmaskData": {
+				"strategy": "facebook"
+			},
+			"servicesConfig": {
+				"passportLogin": {
+					"facebook": {
+						"clientID": "331502413866510",
+						"clientSecret": "1a07a7eb9c9884dc5d148106ede830b2",
+						"callbackURL": "http://local-widget.com/urac/login/success?mode=facebook"
+					}
+				}
+			}
+		}
+	};
+	it("test - init", function (done) {
+		driver.init(req, (error, data) => {
+			assert.ok(data);
+			done();
+		});
+	});
+	it("test - mapProfile", function (done) {
+		let user = {
+			"profile": {
+				"_json":
+					{
+						"first_name": "tony",
+						"last_name": "hage",
+						"email": "antoine@soajs.org"
+					},
+				"id": "123456789"
+			}
+		};
+		driver.mapProfile(user, (error, profile) => {
+			assert.ok(profile);
+			assert.equal(profile.email, "antoine@soajs.org");
+			done();
+		});
+	});
+	it("test - preAuthenticate", function (done) {
+		driver.preAuthenticate(req, (error) => {
+			done();
+		});
+	});
+	it("test - updateConfig", function (done) {
+		driver.updateConfig({"conf": "1"}, (error, config) => {
+			assert.equal(config.conf, "1");
+			done();
+		});
+	});
 });
--- test/unit/lib/passport.js
+++ test/unit/lib/passport.js
@@ -6,96 +6,94 @@ const BL = helper.requireModule('./lib/passport.js');
 const assert = require('assert');
 
 describe("Unit test for: lib - passport", function () {
-    let req = {
-        "soajs": {
-            "inputmaskData": {
-                "strategy": "facebook"
-            },
-            "servicesConfig": {
-                "urac": {
-                    "passportLogin": {
-                        "facebook": {
-                            "clientID": "331502413866510",
-                            "clientSecret": "1a07a7eb9c9884dc5d148106ede830b2",
-                            "callbackURL": "http://local-widget.com/urac/login/success?mode=facebook"
-                        }
-                    }
-                }
-            },
-            "config": helper.requireModule("./config.js")
-        }
-    };
-    let res = {};
-
-    it("test - getDriver - error", function (done) {
-        req.soajs.inputmaskData.strategy = "wrongdriver";
-        BL.getDriver(req, true, function (error, driver) {
-            assert.ok(error.code, "410");
-            done();
-        });
-    });
-    it("test - getDriver", function (done) {
-        req.soajs.inputmaskData.strategy = "facebook";
-        BL.getDriver(req, true, function (error, driver) {
-            assert.ok(driver);
-            done();
-        });
-    });
-    it("test - init - error strategy", function (done) {
-        req.soajs.inputmaskData.strategy = "wrongdriver";
-        BL.init(req, function (error, passport) {
-            assert.ok(error.code, "410");
-            done();
-        });
-    });
-    it("test - init - error servicesConfig", function (done) {
-        req.soajs.inputmaskData.strategy = "facebook";
-        req.soajs.servicesConfig.urac.passportLogin = {};
-        BL.init(req, function (error, passport) {
-            assert.ok(error.code, "410");
-            done();
-        });
-    });
-    it("test - init", function (done) {
-        req.soajs.inputmaskData.strategy = "facebook";
-        req.soajs.servicesConfig.urac.passportLogin = {
-            "facebook": {
-                "clientID": "331502413866510",
-                "clientSecret": "1a07a7eb9c9884dc5d148106ede830b2",
-                "callbackURL": "http://local-widget.com/urac/login/success?mode=facebook"
-            }
-        };
-        BL.init(req, function (error, passport) {
-            assert.ok(passport);
-            done();
-        });
-    });
-    it("test - initAuth - error", function (done) {
-        req.soajs.inputmaskData.strategy = "wrongdriver";
-        let passport = {
-            "authenticate": (authentication, config) => {
-                return (req, res) => {
-                    done();
-                };
-            }
-        };
-        BL.initAuth(req, res, passport, function (error) {
-            assert.ok(error.code, "410");
-            done();
-        });
-    });
-    it("test - initAuth", function (done) {
-        req.soajs.inputmaskData.strategy = "facebook";
-        let passport = {
-            "authenticate": (authentication, config) => {
-                return (req, res) => {
-                    done();
-                };
-            }
-        };
-        BL.initAuth(req, res, passport, function (error) {
-            done();
-        });
-    });
-
+	let req = {
+		"soajs": {
+			"inputmaskData": {
+				"strategy": "facebook"
+			},
+			"servicesConfig": {
+				"passportLogin": {
+					"facebook": {
+						"clientID": "331502413866510",
+						"clientSecret": "1a07a7eb9c9884dc5d148106ede830b2",
+						"callbackURL": "http://local-widget.com/urac/login/success?mode=facebook"
+					}
+				}
+			},
+			"config": helper.requireModule("./config.js")
+		}
+	};
+	let res = {};
+	
+	it("test - getDriver - error", function (done) {
+		req.soajs.inputmaskData.strategy = "wrongdriver";
+		BL.getDriver(req, true, function (error, driver) {
+			assert.ok(error.code, "410");
+			done();
+		});
+	});
+	it("test - getDriver", function (done) {
+		req.soajs.inputmaskData.strategy = "facebook";
+		BL.getDriver(req, true, function (error, driver) {
+			assert.ok(driver);
+			done();
+		});
+	});
+	it("test - init - error strategy", function (done) {
+		req.soajs.inputmaskData.strategy = "wrongdriver";
+		BL.init(req, function (error, passport) {
+			assert.ok(error.code, "410");
+			done();
+		});
+	});
+	it("test - init - error servicesConfig", function (done) {
+		req.soajs.inputmaskData.strategy = "facebook";
+		req.soajs.servicesConfig.passportLogin = {};
+		BL.init(req, function (error, passport) {
+			assert.ok(error.code, "410");
+			done();
+		});
+	});
+	it("test - init", function (done) {
+		req.soajs.inputmaskData.strategy = "facebook";
+		req.soajs.servicesConfig.passportLogin = {
+			"facebook": {
+				"clientID": "331502413866510",
+				"clientSecret": "1a07a7eb9c9884dc5d148106ede830b2",
+				"callbackURL": "http://local-widget.com/urac/login/success?mode=facebook"
+			}
+		};
+		BL.init(req, function (error, passport) {
+			assert.ok(passport);
+			done();
+		});
+	});
+	it("test - initAuth - error", function (done) {
+		req.soajs.inputmaskData.strategy = "wrongdriver";
+		let passport = {
+			"authenticate": (authentication, config) => {
+				return (req, res) => {
+					done();
+				};
+			}
+		};
+		BL.initAuth(req, res, passport, function (error) {
+			assert.ok(error.code, "410");
+			done();
+		});
+	});
+	it("test - initAuth", function (done) {
+		req.soajs.inputmaskData.strategy = "facebook";
+		let passport = {
+			"authenticate": (authentication, config) => {
+				return (req, res) => {
+					done();
+				};
+			}
+		};
+		BL.initAuth(req, res, passport, function (error) {
+			done();
+		});
+	});
+	
 });
