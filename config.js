1daf320fe1929832209d4169850b2721e75aa55f
antoinehage <me@antoinehage.com>
2020-02-21 10:06:27 -0500

    added error 405 and 406 to deliver the failed login reason related to status

--- config.js
+++ config.js
@@ -9,15 +9,17 @@
  */
 
 module.exports = {
-    "model": 'mongo',
-    "hashIterations": 12,
-    "seedLength": 32,
-    "errors": {
-
-        400: "Model error",
-        402: "Problem with the provided password",
-        403: "Unable to log in the user. User not found",
-        404: "Problem with the provided user ID",
-        601: "Model not found"
-    }
+	"model": 'mongo',
+	"hashIterations": 12,
+	"seedLength": 32,
+	"errors": {
+		
+		400: "Model error",
+		402: "Problem with the provided password",
+		403: "Unable to log in the user. User not found",
+		404: "Problem with the provided user ID",
+		405: "Unable to log in the user. User has not activated this account",
+		406: "Unable to log in the user. User is inactive",
+		601: "Model not found"
+	}
 };
