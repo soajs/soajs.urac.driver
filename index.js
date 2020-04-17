4b0b65fdcf53c425ec286b3e72daeb7bb8c66847
antoinehage <me@antoinehage.com>
2020-04-17 15:12:14 -0400

    bumped version to 2.2.3

--- index.js
+++ index.js
@@ -132,7 +132,7 @@ let driver = {
 				}
 				BL.common.comparePasswd(soajs.servicesConfig, input.password, record.password, myConfig, (err, response) => {
 					if (err || !response) {
-						soajs.log.error(err);
+						soajs.log.error(err.message);
 						modelUserObj.closeConnection();
 						return cb({"code": 402, "msg": driverConfig.errors[402]});
 					}
@@ -203,7 +203,7 @@ let driver = {
 				modelUserObj.validateId(data, (err, _id) => {
 					if (err) {
 						modelUserObj.closeConnection();
-						soajs.log.error(err);
+						soajs.log.error(err.message);
 						return cb({"code": 404, "msg": driverConfig.errors[404]});
 					}
 					data.id = _id;
